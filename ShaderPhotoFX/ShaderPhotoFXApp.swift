ceb06148f3e4b44e325590107c7f892018b1e468
Marcello Gonzatto Birkan <mgbirkan@icloud.com>
2024-11-23 02:36:05 -0300

    Initial Commit

---
+++ ShaderPhotoFX/ShaderPhotoFXApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShaderPhotoFXApp.swift
+//  ShaderPhotoFX
+//
+//  Created by Marcello Gonzatto Birkan on 23/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ShaderPhotoFXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
